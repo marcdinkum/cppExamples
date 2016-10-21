ce2ec3b98ecfaa1a105e0a325fc945ae8ff21ca9
ciskavriezenga <ciskavriezenga@gmail.com>
2016-10-21 17:20:17 +0200

    added comments to the main.cpp file 02_dataTypes

--- 01_basics/02_dataTypes/02_character/main.cpp
+++ 01_basics/02_dataTypes/02_character/main.cpp
@@ -20,8 +20,11 @@ int main()
   int noteIndex;
   const char majorScaleC[] = {'c', 'd', 'e', 'f', 'g', 'a', 'b'};
 
+  //ask user for a number from 1 till 7
   cout << "Please enter a whole number from 1 till 7:\n";
   cin >> noteIndex;
+
+  //check if the user entered a number from 1 till 7
   if(noteIndex < 1 && noteIndex > 7){
     cout << "You entered an incorrect number.\n";
     cout << "Please re-run the programm and try again.\n";

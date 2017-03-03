6ea71a6a96cd1af825be7542615966945c40455a
ciskavriezenga <ciskavriezenga@gmail.com>
2017-03-03 20:33:28 +0100

    moved code in ccpp to h file

--- 03_classes/0X_templateClass/Point.h
+++ 03_classes/0X_templateClass/Point.h
@@ -1,3 +1,5 @@
+#include <cmath>
+
 template <class T>
 class Point {
 public:
@@ -6,3 +8,17 @@ public:
   T x;
   T y;
 };
+
+template <class T>
+Point<T>::Point(T x, T y){
+  this->x = x;
+  this->y = y;
+}
+
+template <class T>
+T Point<T>::distance(Point<T> point){
+  T deltaX = point.x - x;
+  T deltaY = point.y - y;
+  T delta = sqrt((deltaX * deltaX) + (deltaY * deltaY));
+  return delta;
+}

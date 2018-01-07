531df531cc451556d1862a29db09794ccbed648f
ciskavriezenga <ciskavriezenga@gmail.com>
2018-01-07 19:14:02 +0100

    added an example with oscillators

--- 08_audioExamples/01_usingJack/02_oscillators/main.cpp
+++ 08_audioExamples/01_usingJack/02_oscillators/main.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
 #include <thread>
 #include "jack_module.h"
-#include "math.h"
-
-#define PI_2 6.28318530717959
-
+#include "sine.h"
 
 
 int main(int argc,char **argv)
 {
   //create a JackModule instance
   JackModule jack;
+
+
+  // init the jack, use program name as JACK client name
+  jack.init(argv[0]);
+
+  //create a Sine instance
+  Sine sine(jack.getSamplerate(), 220, 0);
   //assign a function to the JackModule::onProces
-  jack.onProcess = [](jack_default_audio_sample_t *inBuf,
-     jack_default_audio_sample_t *outBuf, jack_nframes_t nframes, double samplerate) {
-
-    static double phase = 0;
-    static double amplitude = 0.5;
-    static double frequency = 880;
-
+  jack.onProcess = [&sine](jack_default_audio_sample_t *inBuf,
+     jack_default_audio_sample_t *outBuf, jack_nframes_t nframes, double samplerate)
+  {
+    //loop through frames, retrieve sample of sine per frame
     for(int i = 0; i < nframes; i++) {
-      outBuf[i] = amplitude * sin(phase * PI_2 );
-      phase += frequency / samplerate;
+      outBuf[i] = sine.getSample();
+      sine.tick();
     }
 
     return 0;
   };
 
-  // init the jack, use program name as JACK client name
-  jack.init(argv[0]);
+
+  //
   jack.autoConnect();
 
   //keep the program running and listen for user input, q = quit

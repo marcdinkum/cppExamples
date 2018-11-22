1bd680905bf0df2188638bac4fbc08c576029722
ciskavriezenga <ciskavriezenga@gmail.com>
2018-11-22 22:20:29 +0100

    added jackd comment

--- 08_audioExamples/01_usingJack/01_simpleSine/main.cpp
+++ 08_audioExamples/01_usingJack/01_simpleSine/main.cpp
@@ -3,6 +3,14 @@
 #include "jack_module.h"
 #include "math.h"
 
+/*
+ * NOTE: jack2 needs to be installed
+ * jackd invokes the JACK audio server daemon
+ * https://github.com/jackaudio/jackaudio.github.com/wiki/jackd(1)
+ * on mac, you can start the jack audio server daemon in the terminal:
+ * jackd -d coreaudio
+ */
+
 #define PI_2 6.28318530717959
 
 int main(int argc,char **argv)
